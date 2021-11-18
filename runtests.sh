08d560fd189af3bc42fb2ba7f962747cfcbc882d
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2021-11-18 12:24:38 +0530

    Update runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -5,7 +5,7 @@ bin/tovie sis examples/helloworld2.tovie
 
 bin/tovie sis examples/1to10.tovie
 
-bin/tovie sis examples/functionbasic.tovie
+bin/tovie sis examples/fizzbuzz.tovie
 
 read -p "Press any key to continue... " -n1 -s
 clear
